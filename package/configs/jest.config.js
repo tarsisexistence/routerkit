9857ee636843ff53fe18b384f84cb729dcc9f52b
retarsis <tarsis.maksym@gmail.com>
2020-08-30 17:31:43 +0300

    refactor(tsconfig): naming for testing tsconfig

--- package/configs/jest.config.js
+++ package/configs/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: `${__dirname}/../tsconfig.spec.json`
+      tsConfig: `${__dirname}/../tsconfig.test.json`
     }
   },
   moduleNameMapper: {
