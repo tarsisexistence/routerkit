4e94db60dc5dab16d8ebfe7d788a236ade7ddd97
limitofzero <limitofzero@gmail.com>
2020-09-01 16:37:46 +0300

    fix(schematics): add path alias resolving

---
+++ package/src/parse/get-source-file-from-paths.ts
@@ -0,0 +1,62 @@
+import { Project, SourceFile, ts } from 'ts-morph';
+
+interface Alias {
+  originPath: string;
+  withoutAsterisk: string;
+}
+
+// todo think about naming
+export const getSourceFileFromPathsOrThrow = (project: Project, relativeFilePath: string): SourceFile => {
+  const compilerOptions = project.compilerOptions.get();
+  const { paths } = compilerOptions;
+
+  if (paths) {
+    const matchedAliases = Object.keys(paths)
+      .map(alias => ({
+        originPath: alias,
+        withoutAsterisk: alias.replace('*', '')
+      }))
+      .filter(({ withoutAsterisk }) => relativeFilePath.startsWith(withoutAsterisk));
+
+    if (matchedAliases.length) {
+      const baseUrl = compilerOptions.baseUrl || '';
+      return tryGetSourceFileByAliasOrThrow(project, matchedAliases, paths, relativeFilePath, baseUrl);
+    }
+  }
+
+  return project.getSourceFileOrThrow(relativeFilePath);
+};
+
+const tryGetSourceFileByAliasOrThrow = (
+  project: Project,
+  aliases: Alias[],
+  paths: ts.MapLike<string[]>,
+  relativeFilePath: string,
+  baseUrl: string
+): SourceFile => {
+  const maxMatchedAlias = getMaxMatchedAlias(aliases);
+  const resolvedPaths = paths[maxMatchedAlias.originPath];
+
+  const sourceFile = resolvedPaths
+    .map(path => {
+      const pathWithBase = `${baseUrl}/${path.replace('*', '')}`;
+      const resolvedPath = relativeFilePath.replace(maxMatchedAlias.withoutAsterisk, pathWithBase);
+      return project.getSourceFileOrThrow(resolvedPath);
+    })
+    .find(file => !!file);
+
+  if (!sourceFile) {
+    throw new Error(`Can't find file with relative path ${relativeFilePath}`);
+  }
+
+  return sourceFile;
+};
+
+const getMaxMatchedAlias = (aliases: Alias[]): Alias => {
+  return aliases.reduce((maxMatched: Alias, alias: Alias) => {
+    const maxLength = maxMatched.withoutAsterisk.length;
+    const currentAliasLength = alias.withoutAsterisk.length;
+
+    return maxLength > currentAliasLength ? maxMatched : alias;
+  }, aliases[0]);
+};
--- package/src/parse/utils.ts
+++ package/src/parse/utils.ts
@@ -15,6 +15,7 @@ import {
 } from 'ts-morph';
 import { resolve, sep } from 'path';
 import { evaluate } from '@wessberg/ts-evaluator';
+import { getSourceFileFromPathsOrThrow } from './get-source-file-from-paths';
 
 export const getRouteModuleForRootExpressions: (
   routerModuleClass: ClassDeclaration
@@ -141,7 +142,7 @@ const parseRoute = (
 const getLazyModuleDeclaration = (project: Project, loadChildren: RouterKit.Parse.LoadChildren): ClassDeclaration => {
   const { path, moduleName } = loadChildren;
   const pathWithExtension = `${path}.ts`;
-  const sourceFile = project.getSourceFileOrThrow(pathWithExtension);
+  const sourceFile = getSourceFileFromPathsOrThrow(project, pathWithExtension);
   return sourceFile.getClassOrThrow(moduleName);
 };
 
