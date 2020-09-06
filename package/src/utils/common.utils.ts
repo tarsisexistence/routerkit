7fd8f014c00211a8e3212ab9d8ac03ecfcec3b32
retarsis <tarsis.maksym@gmail.com>
2020-09-06 11:02:42 +0300

    feat(log): implement colorful task logging

--- package/src/utils/common.utils.ts
+++ package/src/utils/common.utils.ts
@@ -1,1 +1,17 @@
+import { blue, bold, green, red } from 'chalk';
+
 export const space = (): void => console.log();
+
+export const taskStart = (text: string): string => blue(`${text}...`);
+
+export const taskFail = (text: string): string => red(`${text}.`);
+
+export const taskFinish = (text: string, result: string = ''): string => {
+  const hasResult = result !== '';
+  const color = green;
+  const colorfulText = color(text);
+  const colorfulSign = color(hasResult ? ': ' : '.');
+  const colorfulResult = bold(result);
+
+  return `${colorfulText}${colorfulSign}${colorfulResult}`;
+};
