e84294adc04cafb510fea690bfdd3274b12f3656
retarsis <tarsis.maksym@gmail.com>
2020-09-06 09:22:25 +0300

    feat(logs): improve process log

--- package/src/parse/index.ts
+++ package/src/parse/index.ts
@@ -5,6 +5,7 @@ import { generateRoutesType } from '../generation/generateRoutesType';
 import { generateFile, includeRoutesTypeIntoTsconfig } from '../generation/utils';
 import { findAngularJSON, getProjectAST, getProjectTsconfigPath } from './utils.angular';
 import { getRoutesTypeFilePath, getTypesFileName } from './utils';
+import { space } from '../utils/common.utils';
 
 export function parse(options: RouterKit.Parse.Options): Rule {
   return (tree: Tree) => {
@@ -19,17 +20,22 @@ export function parse(options: RouterKit.Parse.Options): Rule {
     const tsconfigPath = getProjectTsconfigPath(workspace, projectName);
     const projectAST = getProjectAST(tsconfigPath);
     const parsedRoutes = parseRoutes(workspace, projectAST);
+    console.log(`Routes parsed:
+${JSON.stringify(parsedRoutes, null, 4)}
+`);
 
-    if (dryRun) {
-      console.log(parsedRoutes);
-    } else {
+    if (!dryRun) {
       const fileName = getTypesFileName(projectName);
       const filePath = getRoutesTypeFilePath(tsconfigPath, fileName);
       const routesType = generateRoutesType(parsedRoutes, fileName);
-      console.log(routesType);
+      console.log('Type generated.');
       generateFile({ project: projectAST, filePath, output: routesType });
+      console.log(`Output generated: ${filePath}`);
       includeRoutesTypeIntoTsconfig(tsconfigPath, fileName);
+      console.log(`Tsconfig is up-to-date: ${tsconfigPath}`);
     }
+
+    space();
 
     return tree;
   };
