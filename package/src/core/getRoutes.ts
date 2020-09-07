30319e9b4dcf130f8e3f77c185e7730881687dbd
retarsis <tarsis.maksym@gmail.com>
2020-09-07 09:36:52 +0300

    refactor(api): slightly reduced size

--- package/src/core/getRoutes.ts
+++ package/src/core/getRoutes.ts
@@ -1,11 +1,9 @@
 export const getRoutes = <T>(): T => {
   const proxy = new Proxy(['/'], {
     get(target, path): string[] {
-      if (path === Symbol.iterator) {
-        return Array.prototype[Symbol.iterator].bind(target) as string[];
-      }
-
-      return new Proxy([...target, path], this) as string[];
+      return path === Symbol.iterator
+        ? (Array.prototype[Symbol.iterator].bind(target) as string[])
+        : (new Proxy([...target, path], this) as string[]);
     }
   });
 
