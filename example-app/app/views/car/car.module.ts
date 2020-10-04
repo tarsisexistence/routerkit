14475c53ce820a8fc4f0a418d3645ece35b9f3ac
retarsis <tarsis.maksym@gmail.com>
2020-10-04 21:56:31 +0300

    refactor(example): car module

--- example-app/app/views/car/car.module.ts
+++ example-app/app/views/car/car.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { DetailsModule } from '../details/details.module';
 import { CarComponent } from './components/carComponent';
-import { CarHub } from './hub/car.hub';
+
+const carRoutes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: CarComponent
+  },
+  {
+    path: 'engine/:year',
+    pathMatch: 'full',
+    component: CarComponent
+  }
+];
 
 @NgModule({
-  declarations: [CarComponent],
-  imports: [CommonModule, DetailsModule, CarHub]
+  imports: [CommonModule, DetailsModule, RouterModule.forChild(carRoutes)],
+  declarations: [CarComponent]
 })
 export class CarModule {}
--- example-app/app/views/car/hub/car.notes.ts
+++
@@ -1,8 +0,0 @@
-import { Note } from 'routeshub';
-
-export interface CarNotes {
-  root: Note;
-  year: Note;
-}
-
-export const CAR_NOTES_KEY = Symbol();
